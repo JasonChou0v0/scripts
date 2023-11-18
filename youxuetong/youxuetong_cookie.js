5694154fc5ab224294cfb756c90762715cac7158
zhouzhou <76592626+zhouzhouchn@users.noreply.github.com>
2023-11-18 09:06:20 +0800

    Add files via upload

---
+++ youxuetong/youxuetong_cookie.js
@@ -0,0 +1,54 @@
+const cookieName = 'youxuetong'
+const cookieKey = 'zhouzhou_cookie_youxuetong'
+const zhouzhou = init()
+const cookieVal = $request.headers['Cookie']
+if (cookieVal) {
+  if (zhouzhou.setdata(cookieVal, cookieKey)) {
+    zhouzhou.msg(`${cookieName}`, '获取Cookie: 成功', '')
+    zhouzhou.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
+  }
+}
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+zhouzhou.done()
